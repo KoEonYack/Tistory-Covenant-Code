ade3f8267c4fcc1e8e8cfc88dcc242e17357ba7d
koeonyack <covenant.ko@kakao.com>
2021-11-11 22:43:40 +0900

    create junit5 exception basic

---
+++ spring-junit5-exception/src/main/java/covenant/junit5/DoSomething.java
@@ -0,0 +1,8 @@
+package covenant.junit5;
+
+public class DoSomething {
+
+    public static void func() {
+        throw new RuntimeException("some exception message...");
+    }
+}
---
+++ spring-junit5-exception/src/test/java/covenant/junit5/Junit5ExceptionText.java
@@ -0,0 +1,40 @@
+package covenant.junit5;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class Junit5ExceptionText {
+
+    @Test
+    public void junit5에서_exception_테스트_1() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            DoSomething.func();
+        });
+    }
+
+    @Test
+    public void junit5에서_exception_테스트_1_2() {
+        assertThatThrownBy(() -> DoSomething.func()).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void junit5에서_exception_테스트_2() {
+        Throwable exception = assertThrows(RuntimeException.class, () -> {
+            DoSomething.func();
+        });
+        Assertions.assertEquals("some exception message...", exception.getMessage());
+    }
+
+    @Test
+    public void junit5에서_exception_테스트_3() {
+        try {
+            DoSomething.func();
+            Assertions.fail("some exception message...");
+        } catch (RuntimeException e) {
+            Assertions.assertEquals("some exception message...", e.getMessage());
+        }
+    }
+}
